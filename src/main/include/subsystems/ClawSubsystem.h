45263504d432fbded266d9a7dcbe47a801b773f4
HeightsBytes <heightsbytesrobotics@gogarnets.com>
2025-03-05 16:39:54 -0500

    create clawsubsystem.h

--- src/main/include/subsystems/ClawSubsystem.h
+++ src/main/include/subsystems/ClawSubsystem.h
@@ -42,17 +42,85 @@
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
+#include <frc2/command/CommandPtr.h>
+#include <frc/DigitalInput.h>
+#include <rev/SparkMax.h>
+#include <rev/SparkFlex.h>
+#include <units/angle.h>
+#include <units/power.h>
 
 class ClawSubsystem : public frc2::SubsystemBase {
+  enum class IntakeState {
+    kIntaking,
+    kStopped,
+    kEjecting
+  };
+  enum class PivotState {
+    kSwitching,
+    kL1,
+    kL2,
+    kL3,
+    kL4,
+    kCoral
+  };
  public:
   ClawSubsystem();
 
-  /**
-   * Will be called periodically whenever the CommandScheduler runs.
-   */
   void Periodic() override;
 
+  units::degree_t GetPivotAngle();
+  bool AtPivotTarget();
+
+  void SetPivotTarget(PivotState state);
+  void SetIntakeTarget(IntakeState state);
+
+  PivotState GetPivotTarget();
+  PivotState GetPivotActual();
+  IntakeState GetIntakeTarget();
+  IntakeState GetIntakeActual();
+
+  bool GetSensor();
+
+  frc2::CommandPtr Intake(){
+    return this->RunOnce([this]{
+      while (!GetSensor()){
+        SetIntakeTarget(IntakeState::kIntaking);
+      }
+      SetIntakeTarget(IntakeState::kStopped);
+    });
+  }
+
+
+  frc2::CommandPtr SetIntakeTargetCMD(IntakeState state){
+    return this->RunOnce([this, state]{SetIntakeTarget(state); });
+  }
+
+  frc2::CommandPtr SetPivotTargetCMD(PivotState state){
+    return this->RunOnce([this, state]{SetPivotTarget(state); });
+  }
+
  private:
-  // Components (e.g. motor controllers and sensors) should generally be
-  // declared private and exposed only through public methods.
+  units::degree_t StateToOutput(PivotState state) const;
+  double StateToOutput(IntakeState state) const;
+
+  void CheckState();
+
+  std::string ToStr(PivotState state) const;
+  std::string ToStr(IntakeState state) const;
+
+  // motors
+  rev::spark::SparkMax m_pivot;
+  rev::spark::SparkFlex m_intake;
+
+  rev::spark::SparkAbsoluteEncoder m_pivotEncoder;
+  rev::spark::SparkClosedLoopController m_pivotController;
+  // motors
+
+  PivotState m_pivotActual;
+  PivotState m_pivotTarget;
+
+  IntakeState m_intakeTarget;
+
+  frc::DigitalInput m_sensor{10};
+
 };
